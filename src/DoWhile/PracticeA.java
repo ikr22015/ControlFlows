fc1217f65ee9cb9d9996727bab2bea6f7e5bf81a
Imran Kabir <ikr22015@gmail.com>
2018-03-22 20:37:49 -0400

    Created java control flows practice project

---
+++ src/DoWhile/PracticeA.java
@@ -0,0 +1,13 @@
+package DoWhile;
+
+public class PracticeA
+{
+    public static void main(String[] args) {
+        int a = 10;
+
+        do {
+            System.out.println("The value of a is :"+a);
+            a++;
+        }while (a <= 20);
+    }
+}
---
+++ src/While/PracticeA.java
@@ -0,0 +1,14 @@
+package While;
+
+public class PracticeA {
+
+    public static void main(String[] args)
+    {
+        int a = 10;
+
+        while(a < 20)
+        {
+            System.out.println(a);
+        }
+    }
+}
